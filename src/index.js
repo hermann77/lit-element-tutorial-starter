165a24ecd04027c4651aa283c7da5f26aa848453
Hermann Schwarz <schwarz@dipf.de>
2023-03-04 22:11:37 +0100

    Insert the component 'todo-view' with behalf of the router in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './styles.css';
 import './views/todo-view.js';
+import {Router} from '@vaadin/router';
+
+window.addEventListener('load', () => {
+  initRouter();
+});
+
+
+function initRouter() {
+  const router = new Router(document.querySelector('main'));
+  router.setRoutes([
+    {
+      path: '/',
+      component: 'todo-view',
+    },
+    {
+      path: '/stats',
+      component: 'stats-view',
+      action: () => import('./views/stats-view.js'),
+    },
+    {
+      path: '(.*)',
+      component: 'not-found-view',
+      action: () => import('./views/not-found-view.js'),
+    },
+  ]);
+}
